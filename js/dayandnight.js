6b98a396c01a794f8a6eadc643b12d9e1c466f34
Lucas Hoeft <lucashoeft@icloud.com>
2018-08-03 11:07:45 +0200

    Remove redundant code

--- js/dayandnight.js
+++ js/dayandnight.js
@@ -6,7 +6,7 @@ function getStylesheet() {
    document.write("<link rel='stylesheet' href='/stylesheets/css/dark.css' type='text/css'>");
   }
   if (8 <= currentTime&&currentTime < 18) {
-   document.write("<link rel='stylesheet' href='/stylesheets/css/style.css' type='text/css'>");
+   document.write("<link rel='stylesheet' href='/stylesheets/css/light.css' type='text/css'>");
   }
   if (18 <= currentTime&&currentTime <= 24) {
    document.write("<link rel='stylesheet' href='/stylesheets/css/dark.css' type='text/css'>");
